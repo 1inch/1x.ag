58cac4a21146f71f7dfa94ac892b239e730115f0
Nick Kozlov <nk@buttonwallet.com>
2020-02-14 22:15:17 -0700

    cleanup

--- web/src/app/leverage/leverage/step1/step1.component.ts
+++ web/src/app/leverage/leverage/step1/step1.component.ts
@@ -10,80 +10,9 @@ import {Router} from "@angular/router";
   styleUrls: ['./step1.component.scss']
 })
 export class Step1Component implements OnInit {
-
-  hide = true;
-  hideRepeat = true;
-  public passwordForm: FormGroup;
-  passwordsAreValid: boolean;
-  display = false;
-  deactivateForms = false;
-
-
-  constructor( private fb: FormBuilder, private location: Location, private router: Router ) {
-    this.createForm();
-    this.checkValid();
-
-  }
-
-
-  get password() {
-    return this.passwordForm.get('password');
-  }
-
-  get repeatPass() {
-    return this.passwordForm.get('passwordRepeat');
-  }
-
-  createForm() {
-    this.passwordForm = this.fb.group({
-      username: ['BW Account'],
-      password: ['', Validators.required],
-      passwordRepeat: ['', [Validators.required]]
-    });
-  }
-
-  checkValid() {
-    this.passwordsAreValid = this.repeatPass.value === this.password.value && this.repeatPass.value !== '';
-  }
-
-  onPasswordInput() {
-    if (this.passwordForm.hasError('passwordMismatch')) {
-      this.repeatPass.setErrors([{passwordMismatch: true}]);
-    } else {
-      this.repeatPass.setErrors(null);
-    }
+  constructor(  ) {
   }
 
   ngOnInit() {
   }
-
-  onKeydown( event ) {
-    if (event.key === 'Enter' && this.passwordsAreValid) {
-      this.generate();
-    }
-  }
-
-  back() {
-    this.location.back();
-  }
-
-  displayLoader() {
-    this.display = !this.display;
-  }
-
-
-  generate() {
-    of('').pipe(
-      tap(() => this.displayLoader()),
-      tap(() => this.passwordsAreValid = !this.passwordsAreValid),
-      tap(() => this.passwordForm.disable()),
-      delay(5000),
-      tap(() => this.displayLoader()),
-      tap(() => this.passwordsAreValid = !this.passwordsAreValid),
-      tap(() => this.passwordForm.enable()),
-      tap(() => this.router.navigate(['/success/created'])),
-      take(1)
-    ).subscribe();
-  }
-
 }
