5f127aa2bdc2d5456f013309c98c300a6bd67d9c
Nick Kozlov <nk@buttonwallet.com>
2020-02-15 03:33:48 -0700

    add my positions component

---
+++ web/src/app/leverage/leverage/my-positions/my-positions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-positions',
+  templateUrl: './my-positions.component.html',
+  styleUrls: ['./my-positions.component.scss']
+})
+export class MyPositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
