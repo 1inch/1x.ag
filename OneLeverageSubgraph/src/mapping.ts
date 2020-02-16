0723c86cb5027830098138d44a603cba9ebb01d4
Anton Bukov <k06aaa@gmail.com>
2020-02-16 17:51:12 +0300

    Fix subgraph

--- OneLeverageSubgraph/src/mapping.ts
+++ OneLeverageSubgraph/src/mapping.ts
@@ -1,6 +1,6 @@
 import { BigInt, ByteArray, Bytes, crypto } from '@graphprotocol/graph-ts';
 import { ClosePosition as ClosePositionEvent, OpenPosition as OpenPositionEvent } from '../generated/Contract/Contract';
-import { ClosePosition, OpenPosition } from '../generated/schema';
+import { Positions } from '../generated/schema';
 
 function padHex(hex: string, length: i32): string {
     if (hex.startsWith('0x')) {
@@ -9,90 +9,51 @@ function padHex(hex: string, length: i32): string {
     return hex.padStart(length, '0');
 }
 
-function hashOfOpenPosition(
-    owner: Bytes,
-    amount: BigInt,
-    liquidationAmountMin: BigInt,
-    liquidationAmountMax: BigInt,
-    blockNumber: BigInt
+function hashOfPosition(
+    contract: Bytes,
+    owner: Bytes
 ): ByteArray {
     return crypto.keccak256(
         ByteArray.fromHexString(
-            padHex(owner.toHex(), 40) +
-            padHex(amount.toHex().substr(2), 64) +
-            padHex(liquidationAmountMin.toHex().substr(2), 64) +
-            padHex(liquidationAmountMax.toHex().substr(2), 64) +
-            padHex(blockNumber.toHex().substr(2), 64)
-        )
-    );
-}
-
-function hashOfClosePosition(
-    owner: Bytes,
-    blockNumber: BigInt
-): ByteArray {
-    return crypto.keccak256(
-        ByteArray.fromHexString(
-            padHex(owner.toHex(), 40) +
-            padHex(blockNumber.toHex().substr(2), 64)
+            padHex(contract.toHex(), 40) +
+            padHex(owner.toHex(), 40)
         )
     );
 }
 
 export function handleOpenPosition(event: OpenPositionEvent): void {
-    let entity_id = hashOfOpenPosition(
-        event.params.owner,
-        event.params.amount,
-        event.params.liquidationAmountMin,
-        event.params.liquidationAmountMax,
-        event.block.number
+    let entity_id = hashOfPosition(
+        event.params.address,
+        event.params.owner
     ).toHex();
 
-    // Entities can be loaded from the store using a string ID; this ID
-    // needs to be unique across all entities of the same type
-    let entity = OpenPosition.load(entity_id);
+    let entity = Positions.load(entity_id);
 
-    // Entities only exist after they have been saved to the store;
-    // `null` checks allow to create entities on demand
     if (entity == null) {
-        entity = new OpenPosition(entity_id);
+        entity = new Positions(entity_id);
 
-        // Entity fields can be set using simple assignments
-        // entity.updatesCount = BigInt.fromI32(0)
-
-        // Entity fields can be set based on event parameters
+        entity.contract = event.params.address;
         entity.owner = event.params.owner;
         entity.amount = event.params.amount;
-        entity.liquidationAmountMin = event.params.liquidationAmountMin;
-        entity.liquidationAmountMax = event.params.liquidationAmountMax;
+        entity.stopLoss = event.params.stopLoss;
+        entity.takeProfit = event.params.takeProfit;
     }
 
-    // Entities can be written to the store with `.save()`
     entity.save();
 }
 
 export function handleClosePosition(event: ClosePositionEvent): void {
-    let entity_id = hashOfClosePosition(
-        event.params.owner,
-        event.block.number
+    let entity_id = hashOfPosition(
+        event.params.address,
+        event.params.owner
     ).toHex();
 
-    // Entities can be loaded from the store using a string ID; this ID
-    // needs to be unique across all entities of the same type
-    let entity = ClosePosition.load(entity_id);
+    let entity = Positions.load(entity_id);
 
-    // Entities only exist after they have been saved to the store;
-    // `null` checks allow to create entities on demand
     if (entity == null) {
-        entity = new ClosePosition(entity_id);
-
-        // Entity fields can be set using simple assignments
-        // entity.updatesCount = BigInt.fromI32(0)
-
-        // Entity fields can be set based on event parameters
-        entity.owner = event.params.owner;
+        entity = new Positions(entity_id);
+        entity.closed = true;
     }
 
-    // Entities can be written to the store with `.save()`
     entity.save();
 }
